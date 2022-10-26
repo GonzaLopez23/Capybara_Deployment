09fac89febadc505f2a14b5af3b0726a0726e105
facu-01 <facundon.lucero@alumnos.frm.utn.edu.ar>
2022-10-26 00:17:36 -0300

    localidad minor fixes

--- capybara/src/main/java/com/example/capybara/repositories/ProductoRepository.java
+++ capybara/src/main/java/com/example/capybara/repositories/ProductoRepository.java
@@ -1,20 +1,14 @@
 package com.example.capybara.repositories;
 
 import com.example.capybara.entities.Producto;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface ProductoRepository extends BaseRepository<Producto, Long> {
 
-    @Query(value = "SELECT p FROM Producto p WHERE p.nombreProducto LIKE %:filtro% OR p.descripcion LIKE %:filtro%")
-    List<Producto> search(@Param("filtro") String filtro);
-
-    @Query(value = "SELECT p FROM Producto p WHERE p.nombreProducto LIKE %:filtro% OR p.descripcion LIKE %:filtro%")
-    Page<Producto> search(@Param("filtro") String filtro, Pageable pageable);
+//    @Query(value = "SELECT p FROM Producto p WHERE p.nombreProducto LIKE %:filtro% OR p.descripcion LIKE %:filtro%")
+//    List<Producto> search(@Param("filtro") String filtro);
+//
+//    @Query(value = "SELECT p FROM Producto p WHERE p.nombreProducto LIKE %:filtro% OR p.descripcion LIKE %:filtro%")
+//    Page<Producto> search(@Param("filtro") String filtro, Pageable pageable);
 
 
 }
